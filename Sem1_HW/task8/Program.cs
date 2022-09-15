2b214f463b7d603ddfa4f591b63446061ae69b76
YuliaDuk <yule444ka1234@mail.ru>
2022-09-16 00:16:10 +0300

    numbers from 1 to N where N%2==0

---
+++ Sem1_HW/task8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+int count2 = 0;
+if(N > 1)
+{
+    while(count<=N)
+    {
+        Console.WriteLine(count);
+        count = count + 2;
+    }
+}
+else
+{
+    while(count2>=N)
+    {
+    Console.WriteLine(count2);
+    count2 = count2 - 2;
+    }
+}
