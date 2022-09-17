f54cb7014b2c6554d23792b24122e75742f7ad8e
YuliaDuk <yule444ka1234@mail.ru>
2022-09-17 22:33:33 +0300

    switch-case variant

---
+++ Sem2_HW/task15/ver0/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int N = Convert.ToInt32(Console.ReadLine());
+switch(N)
+{
+    case 1:
+    Console.WriteLine("будний день");
+    break;
+    case 2:
+    Console.WriteLine("будний день");
+    break;
+    case 3:
+    Console.WriteLine("будний день");
+    break;
+    case 4:
+    Console.WriteLine("будний день");
+    break;
+    case 5:
+    Console.WriteLine("будний день");
+    break;
+    case 6:
+    Console.WriteLine("выходной день");
+    break;
+    case 7:
+    Console.WriteLine("выходной день");
+    break;
+    default:
+    Console.WriteLine("Введите цифру от 1 до 7");
+    break;
+}
