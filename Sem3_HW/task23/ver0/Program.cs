13c100fc07164941433138c4b26415bacef4da01
YuliaDuk <yule444ka1234@mail.ru>
2022-09-17 22:54:01 +0300

    version with int numbers

---
+++ Sem3_HW/task23/ver0/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N>=1)
+{
+    for (int i = 1; i <= N; i++)
+    {
+    int number = (int)Math.Pow(i, 3);
+    Console.Write($"{number}, ");
+    }
+}
+else if(N<1)
+{
+    for (int i = 1; i >= N; i--)
+    {
+    int number = (int)Math.Pow(i,3);
+    Console.Write($"{number}, ");
+    }
+}
