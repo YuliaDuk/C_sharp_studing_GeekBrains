d666abd86f193ec7caf81c9081cdb0f9716a0335
YuliaDuk <yule444ka1234@mail.ru>
2022-09-17 22:24:05 +0300

    version with string

---
+++ Sem2_HW/task10/ver1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine()!;
+int seconddigit = int.Parse(number[1].ToString());
+int seconddigitforneg = int.Parse(number[2].ToString());
+int length = number.Length;
+if(number.Contains('-'))
+{
+    Console.WriteLine($"Вторая цифра числа {seconddigitforneg}");
+}
+else 
+{
+    Console.WriteLine($"Вторая цифра числа {seconddigit}");
+}
+if(length != 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное или отрицательное число, попробуйте еще раз");
+}
