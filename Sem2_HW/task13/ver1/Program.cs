179e210cd7137b483d41c8027f440c74b8133696
YuliaDuk <yule444ka1234@mail.ru>
2022-09-17 22:31:18 +0300

    version with string

---
+++ Sem2_HW/task13/ver1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine()!;
+int length = number.Length;
+if(length<3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.Write("Третья цифра ");
+    Console.WriteLine(int.Parse(number[2].ToString()));
+}
