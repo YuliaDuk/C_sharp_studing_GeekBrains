f83f2a4256311b2e5059424a0f637652ca807866
YuliaDuk <yule444ka1234@mail.ru>
2022-09-18 13:19:20 +0300

    version with local method

---
+++ Sem4_HW/task27/ver0/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int sumdigit(int number)
+{
+    int sum = 0;
+    if(number>=0)
+    {
+        for (int i = 0; number>0; i++)
+        {
+          sum = sum + number%10;
+          number = number/10;  
+        }
+    }
+    else
+    {
+        number = Math.Abs(number);
+        for (int i = 0; number>=10; i++)
+        {
+            sum = sum + number%10;
+            number = number/10;
+        }
+        sum = sum-number;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр равна {sumdigit(N)}");
