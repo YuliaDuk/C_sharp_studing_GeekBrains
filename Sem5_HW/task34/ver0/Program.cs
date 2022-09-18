ea0d90c088fa290a10c693dedd3ac698b736d27a
YuliaDuk <yule444ka1234@mail.ru>
2022-09-18 13:29:33 +0300

    version with vois FillArray method

---
+++ Sem5_HW/task34/ver0/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+void FillArray(int[] array, int leftrange, int rigthtrange)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(leftrange, rigthtrange+1);
+}
+}
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+FillArray(arr, 100, 999);
+Console.WriteLine('['+ string.Join(", ", arr) + ']');
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    if(arr[i]%2==0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"В массиве {count} четных чисел");
