d4aa78012df04448e1afab65bb76498a191ee0b7
YuliaDuk <yule444ka1234@mail.ru>
2022-09-18 13:20:46 +0300

    solution with string and array

---
+++ Sem4_HW/task27/ver1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+string? number = Console.ReadLine();
+int length = number.Length;
+int[] array = new int [length];
+int sum = 0;
+if(number.Contains('-'))
+{
+    for (int i = 2; i < length; i++)
+    {
+        array[i] = Convert.ToInt32(number[i].ToString());
+        sum = sum + array[i];
+    }
+    array[1] = Convert.ToInt32(number[1].ToString());
+    sum = sum - array[1];
+}
+else
+{
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = Convert.ToInt32(number[i].ToString());
+        sum = sum + array[i];
+    }
+}
+Console.WriteLine($"Сумма цифр заданного числа равна {sum}");
