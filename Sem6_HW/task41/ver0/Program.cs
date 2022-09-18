d66876a378b793aa25101f95070813328a0e9c2a
YuliaDuk <yule444ka1234@mail.ru>
2022-09-18 13:44:47 +0300

    solution - enter numbers with split, then cycle >0

---
+++ Sem6_HW/task41/ver0/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа через запятую: ");
+string text = Console.ReadLine();
+int[] numbers = Array.ConvertAll(text.Split(','), int.Parse);
+int count =0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if( numbers[i]>0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Всего {count} чисел больше 0");
