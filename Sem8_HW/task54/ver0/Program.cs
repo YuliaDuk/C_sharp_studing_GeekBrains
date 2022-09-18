cd2e26db3a52f1e4b683a10e4b73ed30ebb77cc4
YuliaDuk <yule444ka1234@mail.ru>
2022-09-18 14:26:44 +0300

    elements in row in descending order

---
+++ Sem8_HW/task54/ver0/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.WriteLine("Зададим массив размером m*n");
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m,n];
+Console.WriteLine("Исходный массив");
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i,j]= new Random().Next(0,21);
+        Console.Write(matrix[i,j]+ " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+            for (int k = 0; k < n-1; k++)
+            {
+            if(matrix[i,k]<matrix[i,k+1]) 
+            {
+              int temp=matrix[i,k];
+              matrix[i,k]=matrix[i,k+1];
+              matrix[i,k+1]=temp;
+            }
+            }
+    }
+}
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
