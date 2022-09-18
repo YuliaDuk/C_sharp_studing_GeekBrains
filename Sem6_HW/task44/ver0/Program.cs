55255a852e055a353ec84eb17fc1f7fe73b4d6d5
YuliaDuk <yule444ka1234@mail.ru>
2022-09-18 13:50:48 +0300

    Fibonacci numbers

---
+++ Sem6_HW/task44/ver0/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N<=1) Console.WriteLine("Введите другое число");
+else
+{
+    int[] array = new int[N];
+    array[0]=0;
+    array[1] = 1;
+    for (int i = 2; i < N; i++)
+    {
+        array[i] = array[i-1] + array[i-2];
+    }
+    Console.WriteLine(string.Join(" ", array));
+}
