74f3b8c1c88b22820df0f4cd1f35d3379452764d
YuliaDuk <yule444ka1234@mail.ru>
2022-09-18 14:43:11 +0300

    all numbers from N to 1

---
+++ Sem9_HW/task64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число N > 0");
+int n=Convert.ToInt32(Console.ReadLine());
+void Numbers(int N)
+{
+    if(N==0) return;
+    else
+    {
+        Console.Write(N + " ");
+        Numbers(N-1);
+    }
+}
+if(n<=0)
+{
+    Console.WriteLine("Попробуй еще раз");
+}
+else if(n>0)
+{
+Numbers(n);
+}
